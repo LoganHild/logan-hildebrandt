b8bffcdedd615ba3407620e388fb329143f2f4b3
Logan Hildebrandt <hildebrandtlogan@gmail.com>
2021-11-10 02:16:09 -0600

    added some more for layout, added some code to index/app.js, nothing is working on the page right now

---
+++ src/components/pages/About.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <article>
+      <h1>About Me</h1>
+      <p>Full-stack web developer leveraging a background in customer service and care for disabled adults. 
+        Working with disabled adults provided skills in working with others and goal-oriented tasks while developing skills with decision making and problem solving as an independent staff member. 
+        The independence taught skills in how to stay focused, organized,  calm under stress, and to adapt to change.
+        Recently earned a certificate in full-stack development from the University of Minnesota Coding Bootcamp, learning quickly to gain proficiency in HTML, CSS, and JavaScript. As well as gaining
+        proficiency with MySQL, MongoDB, Express, React, and Node. My goal with these newly acquired skills is to create a user-friendly, accessible, and interactive web experience for every user, while aligning with specific goals and exceeding expectations.
+      </p>
+      <p> I am from Cannon Falls, Minnesota, a town of 4,000 or so people, currently residing in Red Wing, Minnesota.
+        I've worked as a DSP for about 6 years taking care of vulnerable adults.
+        My favorite hobby is just being at home and relaxing. I love Dogs, and I try to visit my family dog whenever I have a chance. His name is Teddy.
+        Lately I have really enjoyed working with CSS and getting to see my changes in real time.
+        I enjoy playing Video Games in my free time but lately I have been focused on trying new recipes, exercising more, 
+        and just trying to live a healthier lifestyle in general.
+      </p>
+      <figure>
+        <img class="teddy photo" src="./assets/images/Teddy.jpg" alt="My family dog, Teddy."></img>
+        <img id="family" src="./assets/images/Family.jpg" alt="Myself and Family at brother's football game."></img>
+        <img src="./assets/images/Teddy2.jpg" alt="Another picture of my dog, Teddy" class="teddy photo"></img>
+      </figure>
+    </article>
+  );
+}
