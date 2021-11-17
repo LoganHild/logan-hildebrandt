9c516bb803fff2b8ad40fb02a0a678c55f4a7ca1
Logan Hildebrandt <hildebrandtlogan@gmail.com>
2021-11-17 17:22:52 -0600

    added skills and cover letter from resume to resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,10 +4,68 @@ export default function Resume() {
   return (
     <div>
       <h2>Resume</h2>
-      <p>
-        Thanks for visiting my Portfolio Website! 
-      </p>
       <a href="./assets/newResume.pdf" download="./assets/newResume.pdf">Resume Download</a>
+            
+      <h3>LOGAN HILDEBRANDT</h3>
+      <p>
+        Full-stack web developer leveraging a background in customer service and care for disabled 
+        adults. Working with disabled adults provided skills in working with others and goal-oriented tasks while developing skills with 
+        decision making and problem solving as an independent staff member. About 6 years in total spent in this position taught skills in how to stay focused, 
+        organized,  calm under stress, and to adapt to change. These skills were demonstrated when a client we were serving became physically aggressive towards 
+        staff and other clients. My immediate action was to divide roles between 3 staff for documentation/relocation of other clients, contacting supervisors for 
+        the support needed, and myself working on de-escalating the physically aggressive client. This quick thinking allowed for a quick and safe solution to be 
+        achieved for all parties while being commended by the supervisor and nursing when they arrived. These skills will allow me to find solutions to nearly any
+        problem faced in a new position. Recently earned a certificate in full-stack development from the University of Minnesota Coding Bootcamp, learning quickly 
+        to gain proficiency in HTML, CSS, and JavaScript. My goal with these newly acquired skills is to create a user-friendly, accessible, and interactive web 
+        experience for every user, while aligning with specific goals and exceeding expectations.
+      </p>
+      <h4>Technical Skills</h4>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScrip</li>
+        <li>Node JS</li>
+        <li>JQuery</li>
+        <li>Moment.js</li>
+        <li>Node.js</li>
+        <li>Bootstrap</li>
+        <li>Handlebars.js</li>
+        <li>API Experience</li>
+        <li>NPM Packages</li>
+        <li>Test Driven Development(TDD)</li>
+        <li>Regular Expressions(REGEX)</li>
+        <li>Express</li>
+        <li>MySQL</li>
+        <li>Sequelize</li>
+        <li>MySQLWorkbench</li>
+        <li>MongoDBMongoose</li>
+        <li>Mongoose</li>
+        <li>Robo 3T</li>
+        <li>MongoDB Atlas</li>
+        <li>React</li>
+        <li>RESTful API's</li>
+        <li>Heroku</li>
+        <li>GitHub</li>
+        <li>Figma</li>
+        <li>GraphQl</li>
+      </ul>
+      <h4>Soft Skills</h4>
+      <ul>
+        <li>Organized</li>
+        <li>Attention to Detail</li>
+        <li>Problem Solving</li>
+        <li>Calm Under Stress</li>
+        <li>Adaptability</li>
+        <li>StoryTeller</li>
+        <li>Collaberative</li>
+        <li>Passionate</li>
+        <li>Eager to Learn</li>
+      </ul>
+      <h4>Education</h4>
+      <ul>
+        <li>Certificate, Full Stack Web Development—University of Minnesota	Minneapolis, MN</li>
+        <li>High School Diploma—Cannon Falls High School	Cannon Falls, MN</li>
+      </ul>
     </div>
   );
 }
