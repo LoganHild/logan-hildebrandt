5a1a63f32cafab5725509a06a2d2af7f5528193d
Logan Hildebrandt <hildebrandtlogan@gmail.com>
2021-11-19 17:38:02 -0600

    added github link at bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,6 +11,7 @@ export default function Footer() {
                 <a href="https://www.facebook.com/logan.hildebrandt/" rel="noreferrer" target="_blank"><i class="fab fa-facebook fa-2x social"></i></a>
                 <a href="https://twitter.com/LogHildebrandt" rel="noreferrer" target="_blank"><i class="fab fa-twitter fa-2x social"></i></a>
                 <a href="https://www.linkedin.com/in/logan-hildebrandt-923553210/" rel="noreferrer" target="_blank"><i class="fab fa-linkedin fa-2x social"></i></a>
+                <a href="https://github.com/LoganHild" rel="noreferrer" target="_blank"><i class="fab fa-github fa-2x social"></i></a>
             </div>
         </div>
     )
